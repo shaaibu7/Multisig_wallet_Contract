96b25b3e4b3d6748c7dd27264e1d4207ca2b646c
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-09-12 16:34:27 +0100

    add script interaction for transfer

--- scripts/factoryInteraction.ts
+++ scripts/factoryInteraction.ts
@@ -7,13 +7,10 @@ async function main() {
     const multisigFactoryContractAddress = "0xe1A722603fe84fb84510Aa936d7fAe3B831784F9";
     const multisigFactory = await ethers.getContractAt("MultisigFactory", multisigFactoryContractAddress)
 
-    // const multisigFactoryContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-    // const multisigFactory = await ethers.getContractAt("IMultisigFactory", multisigFactoryContractAddress);
-
+    
     // Deploying multisigWallet using factory
 
     const quorum = 3;
-    // const [ owner, account1, account2, account3 ] = await ethers.getSigners();
     const signer1 = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
     const signer2 = "0xE859ac304020Dd3039082827d2Cbd25979297BDD";
     const signer3 = "0x759CFE087931e6479Cb252Ae62a10Ea30D1d0A39";
@@ -39,14 +36,16 @@ async function main() {
 
     // Interacting with deployed multisigwallet
 
-    // const getdeployedMultisigWallet = getDeployedMultisigWallets[0];
-    // const multisigWalletContract = await ethers.getContractAt("MultisigWalletContract", getdeployedMultisigWallet)
+    const getdeployedMultisigWallet = getDeployedMultisigWallets[0];
+    const multisigWalletContract = await ethers.getContractAt("Multisig", getdeployedMultisigWallet)
 
     
-    // const amount = ethers.parseUnits("100", 18);
-    // multisigWalletContract.transfer()
+    const amount = ethers.parseUnits("100", 18);
+    const tokenTransfer = ethers.parseUnits("1000", 18);
+    web3CXI.transfer(getdeployedMultisigWallet, tokenTransfer);
 
-
+    const transferTx = await multisigWalletContract.transfer(amount, signer1, web3CXITokenAddress);
+    console.log(transferTx);
 
 
 }
